4857d066e6848b3fee7744f2e169047adedcedae
thendokhae18@gmail.com <thendokhae18@gmail.com>
2018-09-25 12:31:13 +0200

    swagger2 added

--- src/main/java/com/springtest/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/springtest/rest/webservices/restfulwebservices/user/User.java
@@ -1,9 +1,13 @@
 package com.springtest.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user")
 public class User {
 
     public User(Integer id, String name, Date birthDate) {
@@ -15,9 +19,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have at at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at at least 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birth date should be before current time")
     private Date birthDate;
 
     public Integer getId() {
